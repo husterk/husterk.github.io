e239a940df2acd91b2ebcfba52b15225c60b1a34
Keith Huster <khuster@curriculumloft.com>
2015-03-08 13:32:53 -0400

    Added honeypot captcha to help prevent spam.

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,7 +17,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            
+
             var sendMsgBtnSelector = $("#btn-send-msg");
             sendMsgBtnSelector.text("Sending...");
             sendMsgBtnSelector.attr("disabled", true);
